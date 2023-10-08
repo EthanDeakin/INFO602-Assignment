12403fd04ff1bbc5c142fb56d4a44edd2af74eb5
EthanDeakin <141538203+EthanDeakin@users.noreply.github.com>
2023-10-09 04:18:51 +1300

    demonstration commit

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; // Replace with your Node.js server URL
+const API_URL = 'http://localhost:3000';
 
 export default {
   // Login user
